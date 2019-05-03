83bd292b877b412e1b83d8ff5e269e4c13cc3caa
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2019-05-03 14:43:47 -0700

    fix tag name

--- Formula/latlon.rb
+++ Formula/latlon.rb
@@ -1,8 +1,7 @@
 class Latlon < Formula
   desc "CLI to get current latitude and longitude"
   homepage "https://github.com/jdxcode/latlon"
-  url "https://github.com/jdxcode/latlon.git",
-      :tag => "1.0.0", :revision => "c04424db099cee8f0f97d3e91a61d15e0d2e68b0"
+  url "https://github.com/jdxcode/latlon.git", :tag => "v1.0.0", :revision => "c04424db099cee8f0f97d3e91a61d15e0d2e68b0"
   head "https://github.com/jdxcode/latlon.git"
 
   depends_on :xcode => ["10.0", :build]
